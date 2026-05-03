c8d9debb3ef3aeef02815efa7c495228fd4f6f95
madhavkochhar20-droid <madhavkochhar20@gmail.com>
2026-05-03 08:46:25 +0530

    Update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interviewai-5y3f.onrender.com",
     credentials:true
 }))
 
