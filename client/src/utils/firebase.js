1ae32cf06612dacb14931d9e4739b3d718b6d3d1
madhavkochhar20-droid <madhavkochhar20@gmail.com>
2026-05-03 09:37:40 +0530

    Fix import path for Firebase authentication

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/Auth"
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: "interviewp-6f6c1.firebaseapp.com",
